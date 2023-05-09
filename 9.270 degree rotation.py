cc9976ad14e257e583396cc040fe8b597dab0a67
Bhargavmandava18 <112241473+Bhargavmandava18@users.noreply.github.com>
2023-05-09 18:10:27 +0530

    Add files via upload

---
+++ 9.270 degree rotation.py
@@ -0,0 +1,18 @@
+#image rotation for 270
+import cv2
+
+
+path = "C://Users\BHARGAV\Pictures\Screenshots\Screenshot (3).png"
+
+
+src = cv2.imread(path)
+
+
+window_name = 'Image'
+
+
+image = cv2.rotate(src, cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+
+cv2.imshow(window_name, image)
+cv2.waitKey(0)
